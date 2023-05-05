6f3fbe63f6602666fa16d61016733d403b5bb46f
Yash chauhan <yash.chauhan@ni.com>
2023-05-05 13:53:33 +0530

    first commit

---
+++ autorunner.py
@@ -0,0 +1,16 @@
+import pyautogui
+from time import sleep
+
+while True:
+    for _ in range(3):
+        pyautogui.moveRel(0, 1000, duration=1)
+        sleep(1)
+        pyautogui.moveRel(1000, 0, duration=1)
+        sleep(1)
+        pyautogui.click()
+        sleep(1)
+        pyautogui.moveRel(0, -1000, duration=1)
+        sleep(1)
+        pyautogui.moveRel(-1000, 0, duration=1)
+        sleep(1)
+    sleep(10)
