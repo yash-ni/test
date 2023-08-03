766f7f5cd5e5fd7ab2fef9281033088f8ecf811b
Yash Chauhan <yash.chauhan@ni.com>
2023-08-03 19:06:37 +0530

    made some changes

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -3,7 +3,7 @@ import subprocess
 
 def sayHello():
     print("Hello World")
-    output = subprocess.run(" ".join(["ls" "-la"]), shell=True, capture_output=True, text=True)
+    output = subprocess.run("ls -la", shell=True, capture_output=True, text=True)
     if output.stderr:
         raise Exception(output.stderr)
     if output.stdout:
