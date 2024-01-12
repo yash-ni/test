a230d279b443dfc3ab328cc0c24423f541210006
yash-ni <131851960+yash-ni@users.noreply.github.com>
2024-01-12 14:50:34 +0530

    Update run_tests.py

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -3,6 +3,8 @@ import subprocess
 
 def sayHello():
     print("Hello World")
+    print("::warning ::This warning is inside python file")
+    print("::warning::This is the second warning from python file")
     output = subprocess.run("sadf", shell=True, capture_output=True, text=True)
     if output.stderr:
         # raise Exception(output.stderr)
